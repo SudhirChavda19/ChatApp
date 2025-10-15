8fb2851aab59b715620ecc26e61888016aaec3d7
sudhir <sudhirahir9329@gmail.com>
2025-10-15 19:03:26 +0530

    server: handle local and deployment private file

--- server/utils/firebaseAdmin.js
+++ server/utils/firebaseAdmin.js
@@ -1,5 +1,8 @@
 const admin = require("firebase-admin");
-const serviceAccount = require("../config/vatchit-544f1-firebase-adminsdk-fbsvc-793eb70047.json");
+const serviceAccount =
+  process.env.NODE_ENV === "production"
+    ? require("/etc/secrets/vatchit-544f1-firebase-adminsdk-fbsvc-793eb70047.json")
+    : require("../config/vatchit-544f1-firebase-adminsdk-fbsvc-793eb70047.json");
 
 admin.initializeApp({
   credential: admin.credential.cert(serviceAccount),
