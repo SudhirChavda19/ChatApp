4e180b3568cbcb3da1bcb961957cf85f5b225d48
sudhir <sudhirahir9329@gmail.com>
2025-10-15 19:23:33 +0530

    server: use upstash redis platform for production redis data store

--- server/redis/redisClient.js
+++ server/redis/redisClient.js
@@ -2,7 +2,7 @@
 const {createClient} = require("redis");
 
 const redisClient = createClient({
-  url: process.env.REDIS_URL || "redis://localhost:6379", // local or managed Redis URL
+  url: process.env.NODE_ENV === "production" ? process.env.REDIS_URL : "redis://localhost:6379",
 });
 
 redisClient.on("error", (err) => console.log("Redis Client Error", err));
