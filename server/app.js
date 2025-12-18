8abbf068ec7a4b73c1fdb33172b904e64d1f4855
sudhir <sudhirahir9329@gmail.com>
2025-12-18 17:24:30 +0530

    resolved server side CORS issue

--- server/app.js
+++ server/app.js
@@ -13,7 +13,7 @@ const MessageRoute = require("./routes/message.route.js");
 dotenv.config();
 const PORT = process.env.PORT;
 const corsOptions = {
-  origin: "https://vatchit.netlify.app/",
+  origin: "https://vatchit.netlify.app",
   methods: ["GET", "PATCH", "POST", "DELETE"],
   credentials: true,
 };
